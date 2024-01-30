8dd9b2d3046267a4dbac13268b9928ac0329f389
Dilip Babu <dilipdavid27@gmail.com>
2024-01-31 00:57:29 +0530

    User Model Creation

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const mongoURI = process.env.MONGO;
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Connection Successful, Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log("Connection Error, Cannot Connect to DB!");
+  });
+
 const app = express();
 
 app.listen(3000, () => {
