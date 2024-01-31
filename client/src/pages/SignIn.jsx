0849b5d026ac6f62bc4d8cc35eb85ad2a42708ed
Dilip Babu <dilipdavid27@gmail.com>
2024-02-01 00:09:06 +0530

    Added Redux Toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 export default function SignIn() {
   const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
 
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -17,7 +24,8 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
+      //setLoading(true);
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -28,16 +36,20 @@ export default function SignIn() {
       const data = await res.json();
 
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
+        // setError(data.message);
+        // setLoading(false);
         return;
       }
-      setLoading(false);
-      setError(null);
+
+      dispatch(signInSuccess(data));
+      // setLoading(false);
+      // setError(null);
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error);
+      dispatch(signInFailure(error.message));
+      // setLoading(false);
+      // setError(error);
     }
   };
   return (
@@ -69,7 +81,7 @@ export default function SignIn() {
       </form>
 
       <div className="flex gap-2 mt-5 font-semibold">
-        <p>Don't have an account yet?</p>
+        <p>Do not have an account yet?</p>
         <Link to={"/sign-up"}>
           <span className="text-blue-700">Sign up</span>
         </Link>
