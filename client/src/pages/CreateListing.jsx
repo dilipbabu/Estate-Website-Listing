a4be444d2304f70371f3759eddb57c26a47cb73e
Dilip Babu <dilipdavid27@gmail.com>
2024-02-07 21:06:09 +0530

    Completed Create List Page Functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -6,53 +6,32 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 export default function CreateListing() {
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
   const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState({
     imageUrls: [],
+    name: "",
+    description: "",
+    address: "",
+    type: "rent",
+    bedrooms: 1,
+    bathrooms: 1,
+    regularPrice: 7000,
+    discountPrice: 0,
+    offer: false,
+    parking: false,
+    furnished: false,
   });
 
   const [imageUploadError, setImageUploadError] = useState(false);
-
   const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   console.log(formData);
-
-  // const handleImageSubmit = (e) => {
-  //   if (files.length === 0) {
-  //     setImageUploadError("At least 1 image is required per listing!");
-  //     return; // Exit early if no files are selected
-  //   }
-
-  //   if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
-  //     setUploading(true);
-  //     setImageUploadError(false);
-  //     const promises = [];
-
-  //     for (let i = 0; i < files.length; i++) {
-  //       promises.push(storeImage(files[i]));
-  //     }
-  //     Promise.all(promises)
-  //       .then((urls) => {
-  //         setFormData({
-  //           ...formData,
-  //           imageUrls: formData.imageUrls.concat(urls),
-  //         });
-
-  //         setImageUploadError(false);
-  //         setUploading(false);
-  //       })
-  //       .catch((error) => {
-  //         setImageUploadError(
-  //           "Image Upload Failed! (Maximum size per image should not exceed 50 MB!)"
-  //         );
-  //         setUploading(false);
-  //       });
-  //   } else {
-  //     setImageUploadError("Max. 6 images per listing!");
-  //     setUploading(false);
-  //   }
-  // };
-
   const handleImageSubmit = (e) => {
     // Reset previous errors
     setImageUploadError(false);
@@ -131,12 +110,76 @@ export default function CreateListing() {
       imageUrls: formData.imageUrls.filter((_, i) => i !== index),
     });
   };
+
+  const handleChange = (e) => {
+    if (e.target.id === "sale" || e.target.id === "rent") {
+      setFormData({
+        ...formData,
+        type: e.target.id,
+      });
+    }
+
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.checked,
+      });
+    }
+
+    if (
+      e.target.type === "number" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError("At least 1 image must be provided!");
+      if (+formData.regularPrice < +formData.discountPrice)
+        return setError("Regular Price must be greater than Discounted Price!");
+
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/listing/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+
+      const data = await res.json();
+      setLoading(false);
+
+      if (data.status === false) {
+        setError(data.message);
+      }
+      navigate(`/lisiting/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-bold text-center my-5 hover:opacity-90 transform hover:scale-90 transition-transform duration-700">
         Create a Listing
       </h1>
-      <form className="flex flex-col sm:flex-row gap-2">
+      <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row gap-2">
         <div className="flex flex-col gap-4 flex-1">
           <input
             type="text"
@@ -146,6 +189,8 @@ export default function CreateListing() {
             maxLength="100"
             minLength="15"
             required
+            onChange={handleChange}
+            value={formData.name}
           />
           <textarea
             type="text"
@@ -153,12 +198,16 @@ export default function CreateListing() {
             className="border p-3 rounded-xl transform hover:scale-90 transition-transform duration-700"
             id="description"
             required
+            onChange={handleChange}
+            value={formData.description}
           />
           <input
             type="text"
             placeholder="Property Address"
             className="border p-3 rounded-xl transform hover:scale-90 transition-transform duration-700"
             id="address"
+            onChange={handleChange}
+            value={formData.address}
           />
           <div className="flex gap-8 flex-wrap">
             <div className="flex gap-2">
@@ -166,6 +215,8 @@ export default function CreateListing() {
                 type="checkbox"
                 id="sale"
                 className="w-5 transform hover:scale-90 transition-transform duration-700"
+                onChange={handleChange}
+                checked={formData.type === "sale"}
               />
               <span>Sell</span>
             </div>
@@ -174,6 +225,8 @@ export default function CreateListing() {
                 type="checkbox"
                 id="rent"
                 className="w-5 transform hover:scale-90 transition-transform duration-700"
+                onChange={handleChange}
+                checked={formData.type === "rent"}
               />
               <span>Rent</span>
             </div>
@@ -182,6 +235,8 @@ export default function CreateListing() {
                 type="checkbox"
                 id="parking"
                 className="w-5 transform hover:scale-90 transition-transform duration-700"
+                onChange={handleChange}
+                checked={formData.parking}
               />
               <span>Parking Facility</span>
             </div>
@@ -190,6 +245,8 @@ export default function CreateListing() {
                 type="checkbox"
                 id="furnished"
                 className="w-5 transform hover:scale-90 transition-transform duration-700"
+                onChange={handleChange}
+                checked={formData.furnished}
               />
               <span>Furnished</span>
             </div>
@@ -198,6 +255,8 @@ export default function CreateListing() {
                 type="checkbox"
                 id="offer"
                 className="w-5 transform hover:scale-90 transition-transform duration-700"
+                onChange={handleChange}
+                checked={formData.offer}
               />
               <span>Offer</span>
             </div>
@@ -207,9 +266,11 @@ export default function CreateListing() {
               <input
                 type="number"
                 className="p-3  border border-gray-300 rounded-xl transform hover:scale-90 transition-transform duration-700"
-                id="bedroom"
+                id="bedrooms"
                 min="1"
                 max="10"
+                onChange={handleChange}
+                value={formData.bedrooms}
                 required
               />
               <p>Bedroom</p>
@@ -218,9 +279,11 @@ export default function CreateListing() {
               <input
                 type="number"
                 className="p-3  border border-gray-300 rounded-xl transform hover:scale-90 transition-transform duration-700"
-                id="baths"
+                id="bathrooms"
                 min="1"
                 max="10"
+                onChange={handleChange}
+                value={formData.bathrooms}
                 required
               />
               <p>Baths</p>
@@ -230,29 +293,35 @@ export default function CreateListing() {
                 type="number"
                 className="p-3  border border-gray-300 rounded-xl transform hover:scale-90 transition-transform duration-700"
                 id="regularPrice"
-                min="1"
-                max="10"
+                min="7000"
+                max="1000000000"
                 required
+                onChange={handleChange}
+                value={formData.regularPrice}
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
                 <span className="text-xs ">(₹ / Month)</span>
               </div>
             </div>
-            <div className="flex items-center gap-2">
-              <input
-                type="number"
-                className="p-3  border border-gray-300 rounded-xl transform hover:scale-90 transition-transform duration-700"
-                id="discountPrice"
-                min="1"
-                max="10"
-                required
-              />
-              <div className="flex flex-col items-center">
-                <p>Discounted Price</p>
-                <span className="text-xs">(₹ / Month)</span>
+            {formData.offer && (
+              <div className="flex items-center gap-2">
+                <input
+                  type="number"
+                  className="p-3  border border-gray-300 rounded-xl transform hover:scale-90 transition-transform duration-700"
+                  id="discountPrice"
+                  min="0"
+                  max="1000000000"
+                  required
+                  onChange={handleChange}
+                  value={formData.discountPrice}
+                />
+                <div className="flex flex-col items-center">
+                  <p>Discounted Price</p>
+                  <span className="text-xs">(₹ / Month)</span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
         <div className="flex flex-col flex-1 gap-4">
@@ -305,11 +374,18 @@ export default function CreateListing() {
             ))}
 
           <button
+            disabled={loading || uploading}
             className="bg-black text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80 
           transform hover:scale-90 transition-transform duration-700"
           >
-            Create Listing
+            {loading ? "Creating....." : "Create Lisiting"}
           </button>
+
+          {error && (
+            <p className="text-red-600 font-medium font-sans text-sm">
+              {error}
+            </p>
+          )}
         </div>
       </form>
     </main>
