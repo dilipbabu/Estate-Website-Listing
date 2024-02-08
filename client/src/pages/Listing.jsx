bba02527f7d2592fca3e04ccb888487eba0822ab
Dilip Babu <dilipdavid27@gmail.com>
2024-02-08 15:27:48 +0530

    Completed Listing Page UI

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,11 +4,22 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from "react-icons/fa";
+
 export default function Listing() {
   SwiperCore.use([Navigation]);
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
   const params = useParams();
   useEffect(() => {
     const fetchListing = async () => {
@@ -31,31 +42,97 @@ export default function Listing() {
     };
     fetchListing();
   }, [params.listingId]);
-  //console.log(loading);
+  console.log(loading);
   return (
     <main>
-      {loading && <p className="text-center my-7 text-2xl spin">Loading....</p>}
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
       {error && (
-        <p className="text-center my-7 text-2xl text-red-700 font-bold">
-          Error Fetching Listing!
-        </p>
+        <p className="text-center my-7 text-2xl">Cannot fetch Listing!</p>
       )}
       {listing && !loading && !error && (
-        <>
+        <div>
           <Swiper navigation>
             {listing.imageUrls.map((url) => (
               <SwiperSlide key={url}>
                 <div
-                  className="h-[50vh] w-full bg-cover bg-center"
+                  className="h-[550px] "
                   style={{
-                    background: `url(${url}) center`,
-                    backgroundSize: "100%",
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
                   }}
                 ></div>
               </SwiperSlide>
             ))}
           </Swiper>
-        </>
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer transform hover:scale-90 transition-transform duration-700">
+            <FaShare
+              className="text-red-600"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+              Link copied!
+            </p>
+          )}
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4 ">
+            <p className="text-4xl font-semibold transform hover:scale-90 transition-transform duration-700">
+              {listing.name} - ₹ {+listing.regularPrice}
+              {/* {listing.offer
+                ? listing.discountPrice.toLocaleString("en-US")
+                : listing.regularPrice.toLocaleString("en-US")}
+              {listing.type === "rent" && " / month"} */}
+            </p>
+            <p className="flex items-center mt-6 gap-2 text-black font-thin  text-xl transform hover:scale-90 transition-transform duration-700">
+              <FaMapMarkerAlt className="text-green-700 text-2xl" />
+              {listing.address}
+            </p>
+            <div className="flex gap-4">
+              <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-xl transform hover:scale-90 transition-transform duration-700">
+                {listing.type === "rent" ? "For Rent" : "For Sale"}
+              </p>
+              {listing.offer && (
+                <p className="bg-green-900 w-full max-w-[200px] max-h-[250px] text-white text-center p-1 rounded-xl transform hover:scale-90 transition-transform duration-700">
+                  Offer Price: ₹{+listing.discountPrice}
+                </p>
+              )}
+            </div>
+            <p className="text-black transform hover:scale-90 transition-transform duration-700">
+              <span className="font-semibold text-black text-xl">
+                Description -{" "}
+              </span>
+              {listing.description}
+            </p>
+            <ul className="text-black font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6 ">
+              <li className="flex items-center gap-1 whitespace-nowrap transform hover:scale-90 transition-transform duration-700">
+                <FaBed className="text-2xl" />
+                {listing.bedrooms > 1
+                  ? `${listing.bedrooms} beds `
+                  : `${listing.bedrooms} bed `}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowraptransform hover:scale-90 transition-transform duration-700 ">
+                <FaBath className="text-2xl" />
+                {listing.bathrooms > 1
+                  ? `${listing.bathrooms} baths `
+                  : `${listing.bathrooms} bath `}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap transform hover:scale-90 transition-transform duration-700">
+                <FaParking className="text-2xl" />
+                {listing.parking ? "Parking spot" : "No Parking"}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap transform hover:scale-90 transition-transform duration-700">
+                <FaChair className="text-2xl" />
+                {listing.furnished ? "Furnished" : "Unfurnished"}
+              </li>
+            </ul>
+          </div>
+        </div>
       )}
     </main>
   );
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -314,7 +314,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
-                <span className="text-xs ">(₹ / Month)</span>
+                {formData.type === "rent" && (
+                  <span className="text-xs">(₹ / Month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -331,7 +333,9 @@ export default function CreateListing() {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
-                  <span className="text-xs">(₹ / Month)</span>
+                  {formData.type === "rent" && (
+                    <span className="text-xs">(₹ / Month)</span>
+                  )}
                 </div>
               </div>
             )}
