2d49f02230974e03c3c21b3d7f8e31f9b7caf99c
Dilip Babu <dilipdavid27@gmail.com>
2024-02-09 03:12:44 +0530

    Completed Pre-Installation Setup

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,21 +3,23 @@ import React from "react";
 export default function About() {
   return (
     <div className="py-20 px-4 max-w-6xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4">About Dilip's Estate Listings</h1>
-      <p className="mb-4 text-black">
+      <h1 className="text-3xl font-bold mb-4 p-10">
+        About Dilip's Estate Listings
+      </h1>
+      <p className="mb-4 text-black text-xl">
         Dilip's Estate Listings is a leading real estate agency that specializes
         in helping clients buy, sell, and rent properties in the most desirable
         neighborhoods. Our team of experienced agents is dedicated to providing
         exceptional service and making the buying and selling process as smooth
         as possible.
       </p>
-      <p className="mb-4 text-black">
+      <p className="mb-4 text-black text-xl">
         Our mission is to help our clients achieve their real estate goals by
         providing expert advice, personalized service, and a deep understanding
         of the local market. Whether you are looking to buy, sell, or rent a
         property, we are here to help you every step of the way.
       </p>
-      <p className="mb-4 text-black">
+      <p className="mb-4 text-black text-xl">
         Our team of agents has a wealth of experience and knowledge in the real
         estate industry, and we are committed to providing the highest level of
         service to our clients. We believe that buying or selling a property
